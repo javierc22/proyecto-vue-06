0d98c0bdff1228fee505da5d1cb41960c47eaf2c
javierc22 <javier.ec.22@gmail.com>
2020-02-25 23:04:41 -0300

    Lazy Generar espera para validar

--- src/views/Validacion.vue
+++ src/views/Validacion.vue
@@ -3,13 +3,13 @@
     <h3>Vuelidate</h3>
     <form @submit.prevent="submit">
       <h4>Email</h4>
-      <input type="email" placeholder="Ingresa un email" class="form-control my-3" v-model="$v.email.$model" :class="{'is-invalid' : $v.email.$error}">
+      <input type="email" placeholder="Ingresa un email" class="form-control my-3" v-model.lazy="$v.email.$model" :class="{'is-invalid' : $v.email.$error}">
       <p class="text-danger" v-if="!$v.email.email">Este email es incorrecto</p>
       <p class="text-danger" v-if="!$v.email.required">Campo requerido</p>
       <p>{{ $v.email }}</p>
 
       <h4>Contraseña</h4>
-      <input type="password" placeholder="Ingresa contraseña" class="form-control my-3" v-model="$v.password.$model" :class="{'is-invalid' : $v.password.$error}">
+      <input type="password" placeholder="Ingresa contraseña" class="form-control my-3" v-model.lazy="$v.password.$model" :class="{'is-invalid' : $v.password.$error}">
       <p class="text-danger" v-if="!$v.password.minLength">Mínimo 5 carácteres</p>
       <input type="password" placeholder="Repite contraseña" class="form-control my-3" v-model="$v.repeatPassword.$model" :class="{'is-invalid' : $v.repeatPassword.$error}">
       <p class="text-danger" v-if="!$v.repeatPassword.sameAsPassword">La contraseña no coincide</p>
