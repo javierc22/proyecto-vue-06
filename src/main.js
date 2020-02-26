30dc1cd7b5e767cdc94f8861adf76e967b208ec2
javierc22 <javier.ec.22@gmail.com>
2020-02-25 22:35:21 -0300

    Instalación y configuración de Vuelidate

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vuelidate from 'vuelidate'
+
+Vue.use(Vuelidate)
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
