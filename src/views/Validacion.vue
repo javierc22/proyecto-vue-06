13585bfe07844d3d826289aac8d9d4aaca7323ef
javierc22 <javier.ec.22@gmail.com>
2020-02-25 23:00:42 -0300

    Validar botón submit

--- src/views/Validacion.vue
+++ src/views/Validacion.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>Vuelidate</h3>
-    <form>
+    <form @submit.prevent="submit">
       <h4>Email</h4>
       <input type="email" placeholder="Ingresa un email" class="form-control my-3" v-model="$v.email.$model" :class="{'is-invalid' : $v.email.$error}">
       <p class="text-danger" v-if="!$v.email.email">Este email es incorrecto</p>
@@ -13,6 +13,10 @@
       <p class="text-danger" v-if="!$v.password.minLength">Mínimo 5 carácteres</p>
       <input type="password" placeholder="Repite contraseña" class="form-control my-3" v-model="$v.repeatPassword.$model" :class="{'is-invalid' : $v.repeatPassword.$error}">
       <p class="text-danger" v-if="!$v.repeatPassword.sameAsPassword">La contraseña no coincide</p>
+
+      <b-button variant="primary" type="submit" :disabled="$v.$invalid">Enviar</b-button>
+
+      {{ $v.$invalid }}
     </form>
   </div>
 </template>
@@ -37,6 +41,18 @@ export default {
     repeatPassword: {
       sameAsPassword: sameAs('password')
     }
-  }
+  },
+  methods: {
+    submit(){
+      console.log('submit!');
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        console.log('Se generó un error!');
+      } else {
+        console.log('Todos los campos correctos');
+        console.log('Enviando información...' + this.$v.email.$model + ' ' + this.$v.password.$model);
+      }
+    }
+  },
 }
 </script>
